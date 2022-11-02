ad5321ebb6f7ae981aa7309833f0a5adfdd30103
Kpraveen07 <Kpraveenk701@gmail.com>
2022-11-02 20:21:42 +0530

    done

---
+++ Addition.java
@@ -0,0 +1,15 @@
+public class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+	   float a=15.5f;
+	   float b=35.5f;
+	   float c=20.5f;
+	   float d=a+b+c;
+	   
+	   System.out.println("Addition=" +d);
+
+	}
+
+}
